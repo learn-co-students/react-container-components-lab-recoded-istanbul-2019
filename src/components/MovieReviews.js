f7253ff93ab3674e69dd7cb879a1f522f909fa30
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-03-05 00:56:04 +0300

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,14 @@
 // Code MovieReviews Here
+import React from 'react';
+
+export default function MovieReviews(props) {
+  return (
+    <div className="review-list">
+      {props.reviews.map(review => {
+        return <div className="review">
+          {review.display_title}
+        </div>
+      })}
+    </div>
+  );
+}
