91168db546ea9d992b98fbc9d2662f88950bfef1
Burak Sürmen <busurmen@gmail.com>
2020-03-05 11:53:04 +0000

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,13 @@
-// Code MovieReviews Here
+import React from 'react'
+
+export default function MovieReviews(props) {
+    return (
+        <div className="review-list">
+            {
+                props.reviews.map((el)=>
+                {return <div key={Math.random()} className="review">{el.display_title}</div>}
+                )
+            }
+        </div>
+    )
+}
