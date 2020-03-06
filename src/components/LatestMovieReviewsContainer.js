3c92c44bcfa327a439037950684a1782d10e63f6
samaromm <samaromm98@gmail.com>
2020-03-06 20:31:50 +0300

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,33 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends Component{
+    constructor(p){
+        super();
+        this.state={
+            reviews:[]
+        }
+    }
+
+    componentDidMount(){
+        fetch(URL)
+        .then(res=>res.json())
+        .then(data=>this.setState({
+            reviews:data.results
+        }))
+    }
+
+    /*handleFetch(data){
+        this.setState({
+            reviews:data.result
+        })
+    }*/
+
+    render(){
+        return(
+            <div className="latest-movie-reviews">
+              <MovieReviews reviews={this.state.reviews}/>
+            </div>
+        )
+    }
+}
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,43 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+
+export default class SearchableMovieReviewsContainer extends Component{
+    constructor(){
+        super()
+        this.state={
+            reviews:[],
+            searchTerm:""
+        }
+    }
+
+    handleChange=(event)=>{
+        this.setState({
+            searchTerm:event.target.value
+        })
+    }
+
+    handleSubmit=(event)=>{
+        event.preventDefault()
+        fetch(URL+ `&query=${this.state.searchTerm}`)
+        .then(res => res.json())
+        .then(data => {
+        this.setState({
+            reviews: data.results,
+        })
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" name="search" onChange={this.handleChange}></input>
+                </form>
+                <div className="searchable-movie-reviews">
+                     <MovieReviews reviews={this.state.reviews} />
+                </div>
+            </div>
+        )
+    }
+}
