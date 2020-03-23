a5313190b044af89e4cccc4a7a0310a7b26c2734
abdurrahman rajab <rajab-2@windowslive.com>
2020-03-23 00:08:34 +0000

    Done.

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,3 +1,12 @@
+import React, { Component } from 'react';
+import 'isomorphic-fetch';
+
+import MovieReviews from './MovieReviews';
+
+const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+const BASE_URL =
+  'https://api.nytimes.com/svc/movies/v2/reviews/search.json?' +
+  `api-key=${NYT_API_KEY}&query=`;
 
 class SearchableMovieReviewsContainer extends Component {
   state = {
@@ -5,7 +14,7 @@ class SearchableMovieReviewsContainer extends Component {
     reviews: []
   };
 
-  handleInputChange = event =>
+  handleSearchInputChange = event =>
     this.setState({ searchTerm: event.target.value });
 
   handleSubmit = event => {
@@ -24,7 +33,8 @@ class SearchableMovieReviewsContainer extends Component {
           <input
             id="search-input"
             type="text"
-            onChange={this.handleInputChange}
+            style={{ width: 300 }}
+            onChange={this.handleSearchInputChange}
           />
           <button type="submit">Submit</button>
         </form>
