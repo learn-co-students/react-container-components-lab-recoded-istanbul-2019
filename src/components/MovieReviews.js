1e7ae065cc8c01944b4423d3fdc3892f457aeecb
yamoonz <yamooonz@gmail.com>
2020-06-01 01:39:03 +0300

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,18 @@
-// Code MovieReviews Here
+import { Component } from "react";// Code MovieReviews Here
+import latestMovieReviewContainer from './latestMovieReviewContainer ';
+import searchableMoviewReviewContainer from './searchableMoviewReviewContainer';
+const MovieReviews = ({reviews})=>{
+    const rev = reviews.map((movie , index )=>{
+        return (
+            <div className = 'review'>
+                <h2>{movie.display_title}</h2>
+                <img src = {movie.multimedia.src} alt='img'/>
+            </div>)
+    })
+    return (
+        <div className = "review-list">
+            {rev}
+        </div>
+    )
+}
+export default MovieReviews
