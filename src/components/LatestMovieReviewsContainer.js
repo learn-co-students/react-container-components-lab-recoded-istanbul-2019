2843e9dcf74078caed38cbd3f09c03afb70ea368
kamalbakour <kamal.bak92@gmail.com>
2020-06-16 01:07:01 +0300

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,9 +1,45 @@
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
-import MovieReviews from './MovieReviews'
-
+import MovieReviews from './MovieReviews';
 const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+const URL = `https://api.nytimes.com/svc/movies/v2/reviews/all.json?api-key=${NYT_API_KEY}`;
+class LatestMovieReviewsContainer extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            reviews: [],
+            headlines: []
+        }
+    }
 
-// Code LatestMovieReviewsContainer Here
+    componentDidMount() {
+        const doFetch = async () => {
+            const response = await fetch(URL);
+            const { results } = await response.json();
+            return this.getFetch(results);
+        };
+        doFetch();
+    };
+    getFetch = (results) => {
+        results.map((item) => {
+            const { headline, summary_short } = item;
+            this.setState({
+                reviews: [
+                    ...this.state.reviews,
+                    summary_short],
+                headlines: [
+                    ...this.state.headlines,
+                    headline
+                ]
+            })
+        })
+    };
+    render() {
+        return (
+            <div className='latest-movie-reviews'>
+               <MovieReviews reviewProp={this.state} />
+            </div>
+        )
+    }
+}
+export default LatestMovieReviewsContainer;
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,21 @@
 // Code MovieReviews Here
+import React from 'react'
+
+export default function MovieReviews({ reviewProp, reviewProp: { headlines, reviews } }) {
+    const headline_and_reviews = headlines.map((item1, index1) => {
+        const matching_review = reviews.find((item2, index2) => index2 === index1);
+        return (
+            <React.Fragment key={index1}>
+                <h2 className='review--headline'>{item1}<br /></h2>
+                <p className='review--summary'>{matching_review}</p>
+            </React.Fragment>
+        )
+    });
+    return (
+        <ul className='review'>
+            <li className='review-list'>
+                {headline_and_reviews}
+            </li>
+        </ul>
+    )
+}
