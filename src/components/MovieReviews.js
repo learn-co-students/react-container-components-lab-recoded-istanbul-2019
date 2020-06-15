085733eedaa1c1a0785ea9482dd2ab203d5e5de8
samah_mustafa <i.samaaah@gmail.com>
2020-06-15 03:19:32 +0300

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,19 @@
 // Code MovieReviews Here
+import React from 'react';
+
+export default function MovieReviews ({reviews}) {
+    const x =  reviews.map((movie , index) => { 
+        return( 
+           <div className ="review">
+             <h3>{movie.display_title}</h3>
+             <img src= {movie.multimedia.src} alt= "img" />
+          </div>
+        )
+     })
+  return (
+     <div  className ="review-list">
+      {x}
+     </div>
+ )
+
+}
