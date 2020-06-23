ade7cc14461f0c9ab0a1a25bb1491258eeb77313
lilas-saleh <lilas.ahmad.saleh@gmail.com>
2020-06-23 23:03:40 +0300

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,10 @@
 // Code MovieReviews Here
+import React from 'react'
+
+export default function MovieReviews(props) {
+    return (
+        <div className='review-list'>
+            {props.reviews.map((review, key) => <div key={key} className='review' > {review.display_title} </div>)}
+        </div>
+    )
+}
