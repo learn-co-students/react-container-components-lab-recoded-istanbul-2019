c80c91dcb4eabc7535a571be2802ac0ab3969604
Burak Selvi <burakselvi858@hotmail.com>
2020-06-25 20:29:57 +0300

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,21 @@
 // Code MovieReviews Here
+import React from "react";
+
+export default function MovieReviews({ reviews }) {
+  return (
+    <div className="review-list">
+      {reviews.map((review) => {
+        return (
+          <div className="review">
+            <p>Headline: {review.headline} </p>
+            <img
+              src={review.multimedia.src}
+              width={review.multimedia.width}
+              height={review.multimedia.height}
+            ></img>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
