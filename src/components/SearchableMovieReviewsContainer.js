3061426bf1a1145f38e5f0d0ea3a8c07be6a9839
refica <refia.candan@gmail.com>
2020-06-27 22:45:25 +0300

    Done.

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,67 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
+const NYT_API_KEY = 'P3oYWjgLX8pfcLQWwRxSYvQgGYTh9vgp';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+
+class SearchableMovieReviewsContainer extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            reviews: [],
+            searchTerm: "",
+        }
+    }
+
+    // fetchData = (e) => {
+    //     e.preventDefault();
+    //     const searchURL = URL + `&query={this.state.searchTerm}`;
+    
+    //     fetch(searchURL)
+    //     .then(res => res.json())
+    //     .then(reviews => {
+    //         this.setState = {
+    //             reviews: reviews.results,
+    //         }
+    //     })
+    // }
+
+    componentDidMount(){
+        fetch(URL + this.state.searchTerm)
+            .then(res => res.json())
+            .then(json => this.setState({
+                reviews: json.results
+            }))
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+    }
+
+    handleSearch = (event) => {
+        this.setState({
+            searchTerm: event.target.value
+        })
+    }
+
+    render() {
+        return (
+            // <div className="searchable-movie-reviews">
+            //     <MovieReviews />
+            // </div>
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" onChange={this.handleSearch}>
+                    </input>
+                </form>
+                <MovieReviews reviews={this.state.reviews} />
+            </div>
+        )
+    }
+};
+
+export default SearchableMovieReviewsContainer;
